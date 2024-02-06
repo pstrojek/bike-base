8acd207ff90556ea2092f8d63984d51341c2e2fd
Pawel Strojek <rapsod@op.pl>
2024-02-06 13:48:42 +0100

    add city input with autocomplete suggestions

--- src/app/pages/bike-search/bike-search.component.ts
+++ src/app/pages/bike-search/bike-search.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Cities } from './cities';
 
 @Component({
   selector: 'app-bike-search',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
   templateUrl: './bike-search.component.html',
-  styleUrl: './bike-search.component.scss'
+  styleUrl: './bike-search.component.scss',
 })
-export class BikeSearchComponent {
+export class BikeSearchComponent implements OnInit {
+  myControl = new FormControl('');
+  options: string[] = Cities;
+  filteredOptions!: Observable<string[]>;
 
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
 }
